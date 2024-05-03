cea01e0e8271cd1d3ed491f83557e500acffe282
Maiorov Aleksandr <av.maiorov@digitaldrilling.ru>
2024-05-03 14:25:54 +0500

    добавлено чтение переменных окружения из .env-файла

--- bot_app/src/db.js
+++ bot_app/src/db.js
@@ -2,6 +2,11 @@ import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
 
 dotenv.config()
+
+console.log('+++++++++++++++++++++++++++++++++++++++++++++++++')
+console.log('+++++++++++++++++++++++++++++++++++++++++++++++++')
+console.log(process.env.POSTGRES_DATABASE)
+
 export const DB = new Sequelize(
     process.env.POSTGRES_DATABASE,
     process.env.POSTGRES_USERNAME,
